7cb50becfcffc7efe638a581ee20b1f5a44200a8
Emmanuel Francis BANIETIKA <sopmusca11@gmail.com>
2025-11-03 22:27:08 +0100

    .
    .

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,15 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
